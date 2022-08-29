67c6766a108481b0dc7c2e219c15fa58887227ef
Jarosław Frydrych <jarek.frydrych@gmail.com>
2022-08-29 15:39:33 +0200

    initial commit

---
+++ apps/lufthansa-task/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lufthansa-task-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'lufthansa-task';
+}
